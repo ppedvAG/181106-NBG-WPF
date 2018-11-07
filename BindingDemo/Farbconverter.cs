099219b4fa8b1c7b49cc320ff866a98e607dfada
Michael Zöhling <michael.zoehling@outlook.com>
2018-11-07 14:37:07 +0100

    Binding - Converter

---
+++ BindingDemo/Farbconverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BindingDemo
+{
+    class Farbconverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string farbe = (string)value;
+            if (farbe == "Rot")
+                return new SolidColorBrush(Colors.Red);
+            //....
+            else
+            {
+                var properties = typeof(Colors).GetProperties();
+
+                var color = properties.FirstOrDefault(x => x.Name.ToLower() == farbe.ToLower());
+                return color != null ? new SolidColorBrush((Color)color.GetValue(typeof(Colors))) : null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
