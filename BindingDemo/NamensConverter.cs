c3636ca775eb50c5e5dc655dac65bbab307cfd7a
Michael Zöhling <michael.zoehling@outlook.com>
2018-11-07 15:24:00 +0100

    Multibinding und Converter

---
+++ BindingDemo/NamensConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BindingDemo
+{
+    class NamensConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values[0] as string + " " + values[1] as string;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+
+            new SolidColorBrush(Color.FromRgb())
+
+        }
+    }
+}
