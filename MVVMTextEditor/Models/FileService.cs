3f750af71be9349f10ab7359f60702b0121f1c04
Michael Zöhling <michael.zoehling@outlook.com>
2018-11-08 10:46:22 +0100

    Editor

---
+++ MVVMTextEditor/Models/FileService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTextEditor.Models
+{
+    class FileService
+    {
+        public string OpenTextFile()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Textdatei | *.txt";
+
+            if(dlg.ShowDialog() == true)
+                return File.ReadAllText(dlg.FileName);
+            throw new InvalidOperationException("Vorgang wurde abgebrochen");
+        }
+
+        public void SaveTextFile(string content)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Textdatei | *.txt";
+
+            if (dlg.ShowDialog() == true)
+                File.WriteAllText(dlg.FileName,content);
+            else
+                throw new InvalidOperationException("Vorgang wurde abgebrochen");
+        }
+    }
+}
---
+++ MVVMTextEditor/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using MVVMTextEditor.Helpers;
+using MVVMTextEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTextEditor.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            service = new FileService();
+            SaveCommand = new RelayCommand(SaveFile);
+            LoadCommand = new RelayCommand(LoadFile);
+        }
+
+        private void LoadFile()
+        {
+            Content = service.OpenTextFile();
+        }
+
+        private void SaveFile()
+        {
+            service.SaveTextFile(Content);
+        }
+
+        private readonly FileService service;
+        public RelayCommand SaveCommand { get; set; }
+        public RelayCommand LoadCommand { get; set; }
+        private string content;
+        public string Content
+        {
+            get => content;
+            set => SetProperty(ref content, value);
+        }
+    }
+}
