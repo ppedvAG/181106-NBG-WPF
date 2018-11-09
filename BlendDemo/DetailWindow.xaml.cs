daaf993d3d72449a5663386daca11fd50205f8aa
Michael Zöhling <michael.zoehling@outlook.com>
2018-11-09 09:29:13 +0100

    ValidationDemo

---
+++ BlendDemo/DetailWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BlendDemo
+{
+    /// <summary>
+    /// Interaction logic for DetailWindow.xaml
+    /// </summary>
+    public partial class DetailWindow : Window
+    {
+        public DetailWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ButtonState == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
---
+++ Validierung/EmailValidationRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Validierung
+{
+    class EmailValidationRule : ValidationRule
+    {
+        Regex r = new Regex(@"[a-zA-Z0-9.]+@[a-zA-Z]+\.[a-zA-Z]{2,3}");
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string eingabe = (string)value;
+            var result = r.Match(eingabe);
+            if (result.Success)
+                return new ValidationResult(true, null);
+            else
+                return new ValidationResult(false, "Ungültige Mailadresse");
+        }
+    }
+}
