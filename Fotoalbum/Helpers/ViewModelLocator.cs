07d78fd357098441fc50dcea639e2e64e9701a6f
Michael Zöhling <michael.zoehling@outlook.com>
2018-11-09 16:11:19 +0100

    Locator um Cleanup erweitert

--- Fotoalbum/Helpers/ViewModelLocator.cs
+++ Fotoalbum/Helpers/ViewModelLocator.cs
@@ -13,6 +13,26 @@ namespace Fotoalbum.Helpers
 
         private FotoService service = new FotoService();
         // Variante: Mehrere Fenster = Mehrere instanzen des selben ViewModels
-        public MainViewModel GetMainViewModel => new MainViewModel(service);
+        private List<MainViewModel> createdInstances = new List<MainViewModel>();
+        public MainViewModel GetMainViewModel
+        {
+            get
+            {
+                createdInstances.Add(new MainViewModel(service));
+                return createdInstances[createdInstances.Count - 1];
+            }
+        }
+
+        public void CleanUp()
+        {
+            foreach (var item in createdInstances)
+            {
+                // item.Dispose();
+
+                // Alternative: Properties manuell leeren und danach:
+
+                // GC.Collect();
+            }
+        }
     }
 }
